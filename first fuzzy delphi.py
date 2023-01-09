41a9eb55d0e5bb59f572621d24a6cada0a765dae
PotyZed <91740001+PotyZed@users.noreply.github.com>
2023-01-09 12:40:29 +0330

    Add files via upload

---
+++ first fuzzy delphi.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import numpy as np
+
+# creating data frame
+df1 = pd.read_excel(r'C:/Users/p.zakeri/Desktop/first phase zed.xlsx')
+mylist1 = []
+count_row = df1.shape[0]
+for i in range(0, count_row):
+    # iloc[i] seperates row i and deleting index 0 is for removing c11 & ....
+    rowlist1 = df1.iloc[i].tolist()
+    del rowlist1[0]
+    # fuzzification
+    rowlist1 = [[0, 0, 0.25] if i == 1 else
+                [0, 0.25, 0.5] if i == 2 else
+                [0.25, 0.5, 0.75] if i == 3 else
+                [0.5, 0.75, 1] if i == 4 else
+                [0.75, 1, 1] if i == 5 else
+                i for i in rowlist1]
+    mylist1.append(rowlist1)
+
+
+# fuzzy dematel first step creating arrays for l,m,u
+X_l = np.zeros((count_row, 153))
+X_m = np.zeros((count_row, 153))
+X_u = np.zeros((count_row, 153))
+
+# replacing with actual value
+for i in range(0, count_row):
+    for j in range(0, 153):
+        a, b, c = mylist1[i][j]
+        X_l[i, j] = a
+        X_m[i, j] = b
+        X_u[i, j] = c
+
+S_l = np.sum(X_l, axis=0)
+S_m = np.sum(X_m, axis=0)
+S_u = np.sum(X_u, axis=0)
+
+M_l = S_l / 25
+M_m = S_m / 25
+M_u = S_u / 25
+
+defuzzied_results = (((M_u - M_l) + (M_m - M_l)) / 3) + M_l
+
+
+
+dff = pd.DataFrame(defuzzied_results)
+dff.to_excel('final.xlsx')
+dff2 = pd.DataFrame(M_l)
+dff2.to_excel('first_l.xlsx')
+dff3 = pd.DataFrame(M_m)
+dff3.to_excel('first_m.xlsx')
+dff4 = pd.DataFrame(M_u)
+dff4.to_excel('first_u.xlsx')
---
+++ second delphi karshenas.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import numpy as np
+
+# creating data frame
+df1 = pd.read_excel(r'C:/Users/p.zakeri/Desktop/second phase zed.xlsx', sheet_name='karshenas')
+
+mylist1 = []
+count_row = df1.shape[0]
+for i in range(0, count_row):
+    # iloc[i] seperates row i and deleting index 0 is for removing c11 & ....
+    rowlist1 = df1.iloc[i].tolist()
+    del rowlist1[0]
+    # fuzzification
+    rowlist1 = [[0, 0, 0.25] if i == 1 else
+                [0, 0.25, 0.5] if i == 2 else
+                [0.25, 0.5, 0.75] if i == 3 else
+                [0.5, 0.75, 1] if i == 4 else
+                [0.75, 1, 1] if i == 5 else
+                i for i in rowlist1]
+    mylist1.append(rowlist1)
+
+
+# fuzzy dematel first step creating arrays for l,m,u
+X_l = np.zeros((count_row, 32))
+X_m = np.zeros((count_row, 32))
+X_u = np.zeros((count_row, 32))
+
+# replacing with actual value
+for i in range(0, count_row):
+    for j in range(0, 32):
+        a, b, c = mylist1[i][j]
+        X_l[i, j] = a
+        X_m[i, j] = b
+        X_u[i, j] = c
+
+S_l = np.sum(X_l, axis=0)
+S_m = np.sum(X_m, axis=0)
+S_u = np.sum(X_u, axis=0)
+
+M_l = S_l / 18
+M_m = S_m / 18
+M_u = S_u / 18
+
+defuzzied_results = (((M_u - M_l) + (M_m - M_l)) / 3) + M_l
+
+
+
+dff = pd.DataFrame(defuzzied_results)
+dff.to_excel('karshenas.xlsx')
+dff2 = pd.DataFrame(M_l)
+dff2.to_excel('karshenas_l.xlsx')
+dff3 = pd.DataFrame(M_m)
+dff3.to_excel('karshenas_m.xlsx')
+dff4 = pd.DataFrame(M_u)
+dff4.to_excel('karshenas_u.xlsx')
